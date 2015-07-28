39968c40d2aa4b2b66b374abeea51978698d2938
bzheng <bohan.zheng@am.sony.com>
2015-07-27 17:14:21 -0700

    add hyperlink to comments

--- javascriptSolutions/array/two_sum.js
+++ javascriptSolutions/array/two_sum.js
@@ -4,8 +4,8 @@
  * numbers such that they add up to the target, where index1 must be less than
  * index2. Please note that your returned answers (both index1 and index2) are
  * not zero-based. <br>You may assume that each input would have exactly one
- * solution. 
- *
+ * solution.<br>
+ * For reference, look here: <a href="https://leetcode.com/problems/two-sum/">leetcode</a>
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
