efa06e0279b31f326a6401fd3897c2dbd24d7b5f
bzheng <bohan.zheng@am.sony.com>
2015-07-27 17:11:20 -0700

    Interesting comments

--- javascriptSolutions/array/two_sum.js
+++ javascriptSolutions/array/two_sum.js
@@ -1,4 +1,11 @@
 /**
+ * Given an array of integers, find two numbers such that they add up to a
+ * specific target number. <br>The function twoSum should return indices of the two
+ * numbers such that they add up to the target, where index1 must be less than
+ * index2. Please note that your returned answers (both index1 and index2) are
+ * not zero-based. <br>You may assume that each input would have exactly one
+ * solution. 
+ *
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -3,12 +3,23 @@ package array;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given an array of integers, find two numbers such that they add up to a
+ * specific target number. <br>The function twoSum should return indices of the two
+ * numbers such that they add up to the target, where index1 must be less than
+ * index2. Please note that your returned answers (both index1 and index2) are
+ * not zero-based. <br>You may assume that each input would have exactly one
+ * solution.
+ * 
+ * @author Bohan Zheng
+ *
+ */
 public class TwoSum {
 	public int[] solution1(int[] nums, int target) {
 		Map<Integer, Integer> table = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
 			if (table.containsKey(target - nums[i])) {
-				return new int[] {table.get(target - nums[i]), i + 1};
+				return new int[] { table.get(target - nums[i]), i + 1 };
 			}
 			table.put(nums[i], i + 1);
 		}
