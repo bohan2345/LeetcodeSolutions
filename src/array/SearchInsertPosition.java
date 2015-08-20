3556e64e2d93973a79b5f2f38cc17712406cb11c
bzheng <bohan.zheng@am.sony.com>
2015-08-20 10:06:46 -0700

    qre

---
+++ src/array/SearchInsertPosition.java
@@ -0,0 +1,20 @@
+package array;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it
+ * would be if it were inserted in order. <br>
+ * You may assume no duplicates in the array. <br>
+ * Here are few examples.<br>
+ * [1,3,5,6], 5 → 2 <br>
+ * [1,3,5,6], 2 → 1 <br>
+ * [1,3,5,6], 7 → 4 <br>
+ * [1,3,5,6], 0 → 0
+ * 
+ * @author Bohan Zheng
+ *
+ */
+public class SearchInsertPosition {
+	public int searchInsert(int[] nums, int target) {
+		return 0;
+	}
+}
