6399b4f0005f4c8749e15ef3fae398825b5aecfb
bzheng <bohan.zheng@am.sony.com>
2015-08-24 10:03:27 -0700

    trdt

---
+++ src/array/CombinationSum.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.List;
+
+/**
+ * Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate
+ * numbers sums to T. <br>
+ * The <b>same</b> repeated number may be chosen from C unlimited number of times. <br>
+ * <b>Note:</b> All numbers (including target) will be positive integers. Elements in a combination (a1, a2, … , ak) must be in
+ * non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak). The solution set must not contain duplicate combinations.<br>
+ * For example, given candidate set 2,3,6,7 and target 7, A solution set is: [7] [2, 2, 3]
+ * 
+ * @author Bohan Zheng
+ *
+ */
+public class CombinationSum {
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		return null;
+	}
+}
--- src/array/SearchInsertPosition.java
+++ src/array/SearchInsertPosition.java
@@ -15,6 +15,26 @@ package array;
  */
 public class SearchInsertPosition {
 	public int searchInsert(int[] nums, int target) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		int left = 0; int right = nums.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] < target) {
+				if (mid + 1 == nums.length || nums[mid + 1] > target) {
+					return mid + 1;
+				}
+				left = mid + 1;
+			} else if (nums[mid] > target) {
+				if (mid == 0 || nums[mid - 1] < target) {
+					return mid;
+				}
+				right = mid - 1;
+			}
+		}
 		return 0;
 	}
 }
