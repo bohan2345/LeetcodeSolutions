b817088e00ee620c4443becf1074a9b1a162ce3c
bzheng <bohan.zheng@am.sony.com>
2015-09-01 14:50:07 -0700

    test

---
+++ src/array/MaximumSubarray.java
@@ -0,0 +1,15 @@
+package array;
+
+/**
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ * <br>
+ * For example, given the array [?2,1,?3,4,?1,2,1,?5,4],<br>
+ * the contiguous subarray [4,?1,2,1] has the largest sum = 6.
+ *
+ * @author Bohan Zheng
+ */
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
+}
