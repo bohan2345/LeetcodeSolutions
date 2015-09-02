15b4b3c03846a926080f3279160a86f9e3c16b4f
bzheng <bohan.zheng@am.sony.com>
2015-09-02 16:37:19 -0700

    asdf

---
+++ src/test/PlusOneTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import array.PlusOne;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * <br>
+ * Created by Bohan Zheng on 9/2/2015.
+ *
+ * @author Bohan Zheng
+ */
+public class PlusOneTest {
+    @Test
+    public void test() {
+        PlusOne test = new PlusOne();
+        int[] digits = {9, 9, 9};
+        int[] expected = {1, 0, 0, 0};
+        assertArrayEquals(expected, test.plusOne(digits));
+    }
+}
