cd4a7841e361789381152bebbc27a807711bb456
bzheng <bohan.zheng@am.sony.com>
2015-09-02 15:57:59 -0700

    asdf

--- src/array/RotateImage.java
+++ src/array/RotateImage.java
@@ -4,13 +4,12 @@ package array;
  * You are given an n x n 2D matrix representing an image. <br>
  * Rotate the image by 90 degrees (clockwise). <br>
  * Could you do this in-place?
- * 
- * @author Bohan Zheng
  *
+ * @author Bohan Zheng
  */
 public class RotateImage {
-	public void rotate(int[][] matrix) {
-		int len = matrix.length - 1;
+    public void rotate(int[][] matrix) {
+        int len = matrix.length - 1;
         for (int level = 0; level <= (len - 1) / 2; level++) {
             for (int shift = level; shift < len - level; shift++) {
                 int tmp = matrix[level][shift];
@@ -20,5 +19,5 @@ public class RotateImage {
                 matrix[shift][len - level] = tmp;
             }
         }
-	}
+    }
 }
---
+++ src/test/MinimumPathSumTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import array.MinimumPathSum;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <br>
+ * Created by Bohan Zheng on 9/2/2015.
+ *
+ * @author Bohan Zheng
+ */
+public class MinimumPathSumTest {
+    @Test
+    public void test() {
+        MinimumPathSum test = new MinimumPathSum();
+        int[][] grid = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+        assertEquals(5, test.minPathSum(grid));
+    }
+}
