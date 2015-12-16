50944f1741b55b388a8cee77d18fb0bc351e0af9
bzheng <bohan.zheng@am.sony.com>
2015-12-15 16:36:08 -0800

    f

---
+++ src/test/array/BestTimetoBuyandSellStockIITest.java
@@ -0,0 +1,26 @@
+package test.array;
+
+import main.array.BestTimetoBuyandSellStockII;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 12/15/2015.
+ *
+ * @author Bohan Zheng
+ */
+public class BestTimetoBuyandSellStockIITest {
+    @Test
+    public void test() {
+        BestTimetoBuyandSellStockII test = new BestTimetoBuyandSellStockII();
+        assertEquals(0, test.maxProfit(new int[]{1}));
+    }
+
+    @Test
+    public void test2() {
+        BestTimetoBuyandSellStockII test = new BestTimetoBuyandSellStockII();
+        assertEquals(1, test.maxProfit(new int[]{1, 2}));
+    }
+}
