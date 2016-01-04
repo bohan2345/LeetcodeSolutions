8f0ca88f7665fefdcb481961f4266d7f7415770c
bzheng <bohan.zheng@am.sony.com>
2016-01-04 15:01:16 -0800

    'fds'

---
+++ src/main/tree/LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,27 @@
+package main.tree;
+
+import main.utils.TreeNode;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 1/4/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class LowestCommonAncestorofaBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p.val > q.val) {
+            return lowestCommonAncestor(root, q, p);
+        }
+        if (root.val >= p.val && root.val <= q.val) {
+            return root;
+        } else if (root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    }
+}
