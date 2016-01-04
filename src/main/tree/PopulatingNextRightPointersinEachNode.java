5c5f56cddf41f598a3123d9acfd011c753143611
bohan2345 <bohan.zheng@gmail.com>
2016-01-04 09:34:57 -0800

    fds

---
+++ src/main/tree/PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,32 @@
+package main.tree;
+
+import main.utils.TreeLinkNode;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 12/30/15.
+ *
+ * @author Bohan Zheng
+ */
+public class PopulatingNextRightPointersinEachNode {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode first = null;
+        while (root != null) {
+            if (first == null) {
+                first = root.left;
+            }
+            if (root.left != null) {
+                root.left.next = root.right;
+            } else {
+                break;
+            }
+            if (root.next != null) {
+                root.right.next = root.next.left;
+                root = root.next;
+            } else {
+                root = first;
+                first = null;
+            }
+        }
+    }
+}
