9632fc68075d13ddb6afcd25d8ede7349f276644
bzheng <bohan.zheng@am.sony.com>
2016-01-04 14:14:52 -0800

    fds

---
+++ src/main/tree/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,34 @@
+package main.tree;
+
+import main.utils.TreeNode;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 1/4/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.equals(p)) {
+            return p;
+        }
+        if (root.equals(q)) {
+            return q;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left == null && right == null) {
+            return null;
+        } else if (left == null) {
+            return right;
+        } else {
+            return left;
+        }
+    }
+}
---
+++ src/test/tree/LowestCommonAncestorofaBinaryTreeTest.java
@@ -0,0 +1,23 @@
+package test.tree;
+
+import main.tree.LowestCommonAncestorofaBinaryTree;
+import main.utils.BinaryTreeUtils;
+import main.utils.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 1/4/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class LowestCommonAncestorofaBinaryTreeTest {
+    @Test
+    public void test() {
+        TreeNode root = BinaryTreeUtils.constructBinaryTree(6, 2, 8, 1, 4, 7, 9, 0, 0, 3, 5);
+        LowestCommonAncestorofaBinaryTree test = new LowestCommonAncestorofaBinaryTree();
+        assertEquals(root.right, test.lowestCommonAncestor(root, root.right.right, root.right.left));
+    }
+}
