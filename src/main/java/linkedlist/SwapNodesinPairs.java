85bd29c3032b92d59cf4016110caa74f3242b4a4
bzheng <bohan.zheng@am.sony.com>
2016-01-26 18:27:54 -0800

    fds

---
+++ src/main/java/linkedlist/SwapNodesinPairs.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+import utils.ListNode;
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * <p>
+ * For example,
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * <p>
+ * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ * <p>
+ * Created by Bohan Zheng on 1/26/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class SwapNodesinPairs {
+    public ListNode swapPairs(ListNode head) {
+        return head;
+    }
+}
---
+++ src/main/java/utils/ListNode.java
@@ -0,0 +1,16 @@
+package utils;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 1/26/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
