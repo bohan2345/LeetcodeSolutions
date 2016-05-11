f890b1c72a08f73d13e8bcf3eb39c3518843a3f9
bzheng <bohan.zheng@am.sony.com>
2016-05-11 15:06:17 -0700

    sdf

---
+++ src/main/java/string/MinimumWindowSubstring.java
@@ -0,0 +1,101 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+ * <p>
+ * For example,
+ * S = "ADOBECODEBANC"
+ * T = "ABC"
+ * Minimum window is "BANC".
+ * <p>
+ * Note:
+ * If there is no such window in S that covers all characters in T, return the empty string "".
+ * <p>
+ * If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+ * <p>
+ * Created by Bohan Zheng on 5/11/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class MinimumWindowSubstring {
+    public String minWindow(String s, String t) {
+        int left = 0, right = 0;
+        Map<Character, Integer> countMap = new HashMap<>();
+        scanString(countMap, t);
+        Map<Character, Integer> tmpMap = new HashMap<>(countMap);
+        while (right < s.length() && !tmpMap.isEmpty()) {
+            char c = s.charAt(right);
+            if (tmpMap.containsKey(c)) {
+                int count = tmpMap.get(c) - 1;
+                if (count == 0) {
+                    tmpMap.remove(c);
+                } else {
+                    tmpMap.put(c, count);
+                }
+            }
+            if (countMap.containsKey(c)) {
+                int count = countMap.get(c) - 1;
+                countMap.put(c, count);
+            }
+            right++;
+        }
+        if (!tmpMap.isEmpty()) {
+            return "";
+        }
+        int minLength = right;
+        int i = 0, j = right;
+        while (right <= s.length()) {
+            left = moveLeft(s, left, countMap);
+            if (minLength > right - left + 1) {
+                i = left - 1;
+                j = right;
+                minLength = j - i;
+            }
+            right = moveRight(s, right, countMap);
+        }
+        return s.substring(i, j);
+    }
+
+    private int moveLeft(String s, int left, Map<Character, Integer> map) {
+        while (left < s.length()) {
+            char c = s.charAt(left);
+            if (map.containsKey(c)) {
+                int count = map.get(c) + 1;
+                map.put(c, count);
+                if (count > 0) {
+                    return left + 1;
+                }
+            }
+            left++;
+        }
+        return left;
+    }
+
+    private int moveRight(String s, int right, Map<Character, Integer> map) {
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                int count = map.get(c) - 1;
+                map.put(c, count);
+                if (count == 0) {
+                    return right + 1;
+                }
+            }
+            right++;
+        }
+        return right + 1;
+    }
+
+    private void scanString(Map<Character, Integer> countMap, String s) {
+        for (char c : s.toCharArray()) {
+            if (countMap.containsKey(c)) {
+                countMap.put(c, countMap.get(c) + 1);
+            } else {
+                countMap.put(c, 1);
+            }
+        }
+    }
+}
---
+++ src/test/java/string/MinimumWindowSubstringTest.java
@@ -0,0 +1,34 @@
+package string;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 5/11/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class MinimumWindowSubstringTest {
+    @Test
+    public void test() {
+        MinimumWindowSubstring test = new MinimumWindowSubstring();
+        assertEquals("BANC", test.minWindow("ADOBECODEBANC", "ABC"));
+    }
+    @Test
+    public void test2() {
+        MinimumWindowSubstring test = new MinimumWindowSubstring();
+        assertEquals("", test.minWindow("ADOBECODEBANC", "ABCZ"));
+    }
+    @Test
+    public void test3() {
+        MinimumWindowSubstring test = new MinimumWindowSubstring();
+        assertEquals("cabd", test.minWindow("abcabdebac", "cda"));
+    }
+    @Test
+    public void test4() {
+        MinimumWindowSubstring test = new MinimumWindowSubstring();
+        assertEquals("adobecodeba", test.minWindow("adobecodebanc", "abcda"));
+    }
+}
