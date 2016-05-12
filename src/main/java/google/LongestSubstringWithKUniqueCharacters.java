972830c1e6304120727ac516ca5b0fbf12f19073
bzheng <bohan.zheng@am.sony.com>
2016-05-12 14:41:11 -0700

    fds

---
+++ src/main/java/google/LongestSubstringWithKUniqueCharacters.java
@@ -0,0 +1,50 @@
+package google;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a string str and a number k, find the longest substring contains at most k unique characters.
+ * For example,
+ * Given str = "eceha", k = 2
+ * Return "ece"
+ * <p>
+ * Created by Bohan Zheng on 5/12/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class LongestSubstringWithKUniqueCharacters {
+    public String longestSubstring(String str, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0;
+        int maxLength = 0, a = 0, b = 0;
+        while (right < str.length()) {
+            if (map.keySet().size() > k) {
+                //moveLeft
+                char c = str.charAt(left);
+                int count = map.get(c) - 1;
+                if (count == 0) {
+                    map.remove(c);
+                } else {
+                    map.put(c, count);
+                }
+                left++;
+            } else {
+                //moveRight
+                char c = str.charAt(right);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                } else {
+                    map.put(c, 1);
+                }
+                if (maxLength < right - left) {
+                    maxLength = right - left;
+                    a = right;
+                    b = left;
+                }
+                right++;
+            }
+        }
+        return str.substring(b, a);
+    }
+}
---
+++ src/test/java/google/LongestSubstringWithKUniqueCharactersTest.java
@@ -0,0 +1,31 @@
+package google;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 5/12/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class LongestSubstringWithKUniqueCharactersTest {
+    @Test
+    public void test() {
+        LongestSubstringWithKUniqueCharacters test = new LongestSubstringWithKUniqueCharacters();
+        assertEquals("ece", test.longestSubstring("eceab", 2));
+    }
+
+    @Test
+    public void test2() {
+        LongestSubstringWithKUniqueCharacters test = new LongestSubstringWithKUniqueCharacters();
+        assertEquals("eceeeee", test.longestSubstring("eceeeeeab", 2));
+    }
+
+    @Test
+    public void test3() {
+        LongestSubstringWithKUniqueCharacters test = new LongestSubstringWithKUniqueCharacters();
+        assertEquals("eeeee", test.longestSubstring("eceeeeeab", 1));
+    }
+}
