5f4d49d6f7b96228d48b62b2f84757f630812c43
bzheng <bohan.zheng@am.sony.com>
2016-05-24 14:17:40 -0700

    fds

---
+++ src/main/java/string/RegularExpressionMatching.java
@@ -0,0 +1,56 @@
+package string;
+
+/**
+ * Implement regular expression matching with support for '.' and '*'.
+ * <p>
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * <p>
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ * <p>
+ * Some examples:
+ * isMatch("aa","a") → false
+ * isMatch("aa","aa") → true
+ * isMatch("aaa","aa") → false
+ * isMatch("aa", "a*") → true
+ * isMatch("aa", ".*") → true
+ * isMatch("ab", ".*") → true
+ * isMatch("aab", "c*a*b") → true
+ * <p>
+ * Created by Bohan Zheng on 5/23/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        boolean[][] table = new boolean[p.length() + 1][s.length() + 1];
+        table[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*') {
+                table[i + 1][0] = table[i][0];
+            } else if (i + 1 < p.length() && p.charAt(i + 1) == '*') {
+                table[i + 1][0] = table[i][0];
+            }
+        }
+        for (int i = 0; i < p.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                char pc = p.charAt(i), sc = s.charAt(j);
+                if (pc == '*') {
+                    // f(i, j) = f(i - 1. j)
+                    table[i + 1][j + 1] = table[i][j + 1];
+                } else if (i + 1 < p.length() && p.charAt(i + 1) == '*') {
+                    // f(i, j) = f(i, j - 1) && (p[i] == s[j] || p[i] == .) , try to match with p[i]
+                    //         = f(i - 1, j)                                  skip p[i]
+                    table[i + 1][j + 1] = (table[i + 1][j] && (pc == '.' || pc == sc)) || table[i][j + 1];
+                } else {
+                    // f(i, j) = f(i - 1, j - 1) && (p[i] == s[j] || p[i] == .)
+                    table[i + 1][j + 1] = table[i][j] && (pc == '.' || pc == sc);
+                }
+            }
+        }
+        return table[p.length()][s.length()];
+    }
+}
---
+++ src/test/java/string/RegularExpressionMatchingTest.java
@@ -0,0 +1,71 @@
+package string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 5/23/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class RegularExpressionMatchingTest {
+    @Test
+    public void test() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertFalse(test.isMatch("aa", "a"));
+    }
+
+    @Test
+    public void test2() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertTrue(test.isMatch("aa", "aa"));
+    }
+
+    @Test
+    public void test3() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertFalse(test.isMatch("abb", "b*"));
+    }
+
+    @Test
+    public void test4() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertTrue(test.isMatch("aaa", "a*"));
+    }
+
+    @Test
+    public void test5() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertTrue(test.isMatch("aaa", ".*"));
+    }
+
+    @Test
+    public void test6() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertTrue(test.isMatch("aab", "a*a*b*"));
+    }
+
+    @Test
+    public void test7() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertTrue(test.isMatch("aab", ".*"));
+    }
+    @Test
+    public void test8() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertFalse(test.isMatch("aab", ""));
+    }
+    @Test
+    public void test9() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertTrue(test.isMatch("", "a*"));
+    }
+    @Test
+    public void test10() {
+        RegularExpressionMatching test = new RegularExpressionMatching();
+        assertTrue(test.isMatch("a", "ab*"));
+    }
+}
