1680a59a61e5dfa74f98c9f7c8c54edab6c0af66
bzheng <bohan.zheng@am.sony.com>
2016-06-13 08:50:25 -0700

    fds

---
+++ src/test/java/google/TriangleCountTest.java
@@ -0,0 +1,21 @@
+package google;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 6/13/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class
+TriangleCountTest {
+    @Test
+    public void test() {
+        TriangleCount test = new TriangleCount();
+        int[] nums = {1, 2, 3, 4, 5};
+        assertEquals(3, test.count(nums));
+    }
+}
