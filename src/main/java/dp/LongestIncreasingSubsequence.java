333da96f880cdf67500af47a664ca6c18b2720be
bzheng <bohan.zheng@am.sony.com>
2016-06-21 16:40:10 -0700

    fds

---
+++ src/main/java/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,45 @@
+package dp;
+
+/**
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ * <p>
+ * For example,
+ * Given [10, 9, 2, 5, 3, 7, 101, 18],
+ * The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4.
+ * Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+ * <p>
+ * Your algorithm should run in O(n^2) complexity.
+ * <p>
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ * <p>
+ * Created by Bohan Zheng on 6/21/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class LongestIncreasingSubsequence {
+    /**
+     * f(i) denotes the longest subsequence end at nums[i]
+     * f(i) = max(f(k) + 1), 0 < k < i, if nums[i] > nums[k]
+     */
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] f = new int[nums.length];
+        f[0] = 1;
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int max = f[0];
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    max = Math.max(f[j] + 1, max);
+                } else {
+                    max = Math.max(max, 1);
+                }
+            }
+            f[i] = max;
+            res = Math.max(f[i], res);
+        }
+        return res;
+    }
+}
---
+++ src/test/java/dp/LongestIncreasingSubsequenceTest.java
@@ -0,0 +1,25 @@
+package dp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 6/21/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class LongestIncreasingSubsequenceTest {
+    @Test
+    public void test() {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        assertEquals(4, new LongestIncreasingSubsequence().lengthOfLIS(nums));
+    }
+
+    @Test
+    public void test2() {
+        int[] nums = {1, 3, 6, 7, 9, 4, 10, 5, 6};
+        assertEquals(6, new LongestIncreasingSubsequence().lengthOfLIS(nums));
+    }
+}
