b3b00e8d5b0ac98473e3df2ebc619cbbb73ce541
bzheng <bohan.zheng@am.sony.com>
2016-06-30 18:27:27 -0700

    fds

---
+++ src/main/java/google/DetectCycle.java
@@ -0,0 +1,15 @@
+package google;
+
+import java.util.List;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 6/30/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class DetectCycle {
+    public boolean detectCycle(List<int[]> edges) {
+        return false;
+    }
+}
---
+++ src/test/java/google/DetectCycleTest.java
@@ -0,0 +1,10 @@
+package google;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 6/30/2016.
+ *
+ * @author Bohan Zheng
+ */
+public class DetectCycleTest {
+}
