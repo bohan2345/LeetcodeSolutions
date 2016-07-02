81b201dbc4dec7086d5cd705dba96b077852bd72
bohan2345 <bohan.zheng@gmail.com>
2016-07-02 14:07:44 -0700

    sdf

--- src/main/java/google/Itoa.java
+++ src/main/java/google/Itoa.java
@@ -1,10 +1,20 @@
 package google;
 
 /**
+ * itoa() function in C language converts int data type to string data type.
  * <p>
  * Created by Bohan Zheng on 7/2/16.
  *
  * @author Bohan Zheng
  */
 public class Itoa {
+    public String itoa(int num, int base) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        while (num != 0) {
+            sb.insert(0, chars[num % base]);
+            num = num / base;
+        }
+        return sb.toString();
+    }
 }
---
+++ src/test/java/google/ItoaTest.java
@@ -0,0 +1,31 @@
+package google;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 7/2/16.
+ *
+ * @author Bohan Zheng
+ */
+public class ItoaTest {
+    @Test
+    public void test() {
+        Itoa test = new Itoa();
+        assertEquals(Integer.toBinaryString(123), test.itoa(123, 2));
+    }
+
+    @Test
+    public void test2() {
+        Itoa test = new Itoa();
+        assertEquals(Integer.toOctalString(123), test.itoa(123, 8));
+    }
+
+    @Test
+    public void test3() {
+        Itoa test = new Itoa();
+        assertEquals(Integer.toHexString(123).toUpperCase(), test.itoa(123, 16));
+    }
+}
