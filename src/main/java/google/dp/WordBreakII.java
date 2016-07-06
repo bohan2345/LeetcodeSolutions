8be995a862aece08a91b6256260bbbd2e5753853
bohan2345 <bohan.zheng@gmail.com>
2016-07-06 00:01:10 -0700

    gi

---
+++ src/main/java/google/dp/WordBreakII.java
@@ -0,0 +1,49 @@
+package google.dp;
+
+import java.util.*;
+
+/**
+ * Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+ * <p>
+ * Return all such possible sentences.
+ * <p>
+ * For example, given
+ * s = "catsanddog",
+ * dict = ["cat", "cats", "and", "sand", "dog"].
+ * <p>
+ * A solution is ["cats and dog", "cat sand dog"].
+ * <p>
+ * Created by Bohan Zheng on 7/5/16.
+ *
+ * @author Bohan Zheng
+ */
+public class WordBreakII {
+
+    public List<String> wordBreakIII(String s, Set<String> dict) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> res = search(s, dict, map);
+        return res;
+    }
+
+    private List<String> search(String s, Set<String> dict, Map<String, List<String>> cache) {
+        if (cache.containsKey(s)) return cache.get(s);
+        List<String> res = new ArrayList<>();
+        if (dict.contains(s)) {
+            res.add(s);
+        }
+        for (int i = 1; i < s.length(); i++) {
+            String sub = s.substring(i);
+            if (dict.contains(sub)) {
+                List<String> tmp = search(s.substring(0, i), dict, cache);
+                sub = " " + sub;
+                for (String tmpstr : tmp) {
+                    String str = tmpstr + sub;
+                    res.add(str);
+                }
+            }
+        }
+        cache.put(s, res);
+        return res;
+    }
+}
---
+++ src/test/java/google/dp/WordBreakIITest.java
@@ -0,0 +1,50 @@
+package google.dp;
+
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 7/5/16.
+ *
+ * @author Bohan Zheng
+ */
+public class WordBreakIITest {
+    @Test
+    public void test() {
+        WordBreakII test = new WordBreakII();
+        Set<String> s = new HashSet<>();
+        s.addAll(Arrays.asList("cat", "cats", "and", "sand", "dog"));
+
+        assertThat(test.wordBreakIII("catsanddog", s), IsIterableContainingInAnyOrder.containsInAnyOrder("cats and dog", "cat sand dog"));
+    }
+
+//    @Test
+//    public void test2() {
+////        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+////                ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+//        WordBreakII test = new WordBreakII();
+//        Set<String> s = new HashSet<>();
+//        s.addAll(Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"));
+//
+//        assertTrue(test.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", s).isEmpty());
+//    }
+
+    @Test
+    public void test3() {
+//        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+//                ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+        WordBreakII test = new WordBreakII();
+        Set<String> s = new HashSet<>();
+        s.addAll(Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"));
+
+        assertTrue(test.wordBreakIII("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", s).isEmpty());
+    }
+}
