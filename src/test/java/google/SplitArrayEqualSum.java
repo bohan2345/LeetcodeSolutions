501fd6b91003c1c36180f8779d55e17829c7bff9
bohan2345 <bohan.zheng@gmail.com>
2016-07-09 15:32:35 -0700

    sdf

--- src/test/java/google/SplitArrayEqualSum.java
+++ src/test/java/google/SplitArrayEqualSum.java
@@ -7,5 +7,19 @@ package google;
  * @author Bohan Zheng
  */
 public class SplitArrayEqualSum {
-
+    public int split(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        int half = 0;
+        for (int i = 0; i < nums.length; i++) {
+            half += nums[i];
+            sum -= nums[i];
+            if (half == sum) {
+                return i;
+            }
+        }
+        throw new RuntimeException("no such index");
+    }
 }
