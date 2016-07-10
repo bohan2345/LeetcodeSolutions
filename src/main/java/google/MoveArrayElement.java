175375ca6f98d4685a7887b7d8b34d57d7072597
bohan2345 <bohan.zheng@gmail.com>
2016-07-10 12:01:17 -0700

    sdf

---
+++ src/main/java/google/MoveArrayElement.java
@@ -0,0 +1,61 @@
+package google;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 7/10/16.
+ *
+ * @author Bohan Zheng
+ */
+public class MoveArrayElement {
+    public int moveBack(int[] array) {
+        boolean[] visited = new boolean[array.length];
+        List<Integer> circles = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            if (!visited[i]) {
+                circles.add(findCircle(array, i, visited));
+            }
+        }
+
+        return (int) lcm(circles);
+    }
+
+    private int findCircle(int[] array, int i, boolean[] visited) {
+        int next = array[i];
+        visited[i] = true;
+        int length = 1;
+        while (next != i && length <= array.length) {
+            length++;
+            visited[next] = true;
+            next = array[next];
+        }
+        if (length < array.length) {
+            return length;
+        } else {
+            return next == i ? length : -1;
+        }
+    }
+
+    private long lcm(long a, long b) {
+        long multiple = a * b;
+        return multiple / gcd(a, b);
+    }
+
+    private long lcm(List<Integer> list) {
+        long result = 1;
+        for (Integer aList : list) result = lcm(result, aList);
+        return result;
+    }
+
+    private long gcd(long a, long b) {
+        while (b > 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
---
+++ src/test/java/google/MoveArrayElementTest.java
@@ -0,0 +1,34 @@
+package google;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 7/10/16.
+ *
+ * @author Bohan Zheng
+ */
+public class MoveArrayElementTest {
+    @Test
+    public void test() {
+        MoveArrayElement test = new MoveArrayElement();
+        int[] array = {1, 2, 3, 0};
+        assertEquals(4, test.moveBack(array));
+    }
+
+    @Test
+    public void test2() {
+        MoveArrayElement test = new MoveArrayElement();
+        int[] array = {0, 1, 2, 3};
+        assertEquals(1, test.moveBack(array));
+    }
+
+    @Test
+    public void test3() {
+        MoveArrayElement test = new MoveArrayElement();
+        int[] array = {1, 0, 3, 4, 2};
+        assertEquals(6, test.moveBack(array));
+    }
+}
