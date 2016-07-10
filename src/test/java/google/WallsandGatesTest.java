84da4525b079d2f46336cdce664285c82b8c4747
bohan2345 <bohan.zheng@gmail.com>
2016-07-10 13:51:00 -0700

    sdf

---
+++ src/test/java/google/WallsandGatesTest.java
@@ -0,0 +1,20 @@
+package google;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 7/10/16.
+ *
+ * @author Bohan Zheng
+ */
+public class WallsandGatesTest {
+    @Test
+    public void test() {
+        WallsandGates test = new WallsandGates();
+        int[][] array = {{2147483647, -1, 0, 2147483647}, {2147483647, 2147483647, 2147483647, -1},
+                {2147483647, -1, 2147483647, -1}, {0, -1, 2147483647, 2147483647}};
+        test.wallsAndGates(array);
+
+    }
+}
