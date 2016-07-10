5d34f6689c8a3c5de1ff82bd02be6d7123a588cd
bohan2345 <bohan.zheng@gmail.com>
2016-07-10 12:13:37 -0700

    sdf

---
+++ src/test/java/google/RandomArrayTest.java
@@ -0,0 +1,20 @@
+package google;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 7/10/16.
+ *
+ * @author Bohan Zheng
+ */
+public class RandomArrayTest {
+    @Test
+    public void test() {
+        RandomArray test = new RandomArray();
+        int[] r = test.randomArray(9);
+        assertEquals(0, 0);
+    }
+}
