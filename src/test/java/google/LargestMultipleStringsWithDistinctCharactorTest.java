1fdad404f71dadb51336d44c54157661e58b019c
bohan2345 <bohan.zheng@gmail.com>
2016-07-10 23:52:30 -0700

    sdf

---
+++ src/test/java/google/LargestMultipleStringsWithDistinctCharactorTest.java
@@ -0,0 +1,20 @@
+package google;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 7/10/16.
+ *
+ * @author Bohan Zheng
+ */
+public class LargestMultipleStringsWithDistinctCharactorTest {
+    @Test
+    public void test() {
+        String[] strs = {"asdfasd", "ghjk", "zxcvzxcv", "zxcvzxcc", "x"};
+        LargestMultipleStringsWithDistinctCharactor test = new LargestMultipleStringsWithDistinctCharactor();
+        assertEquals(56, test.max(strs));
+    }
+}
