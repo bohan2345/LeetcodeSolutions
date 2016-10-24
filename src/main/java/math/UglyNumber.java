1386e4420f117df0ea0722a320a5bfc5a5fce767
bohan2345 <bohan.zheng@gmail.com>
2016-10-23 20:13:18 -0700

    sdf

---
+++ src/main/java/math/UglyNumber.java
@@ -0,0 +1,25 @@
+package math;
+
+/**
+ * <p>
+ * Created by Bohan Zheng on 10/23/16.
+ *
+ * @author Bohan Zheng
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num < 1) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
