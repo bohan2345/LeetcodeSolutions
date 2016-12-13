0bdcf2293c3bd088ad24d2321636dcf6008bb5b1
bohan2345 <bohan.zheng@gmail.com>
2016-12-12 19:59:05 -0800

    sdf

--- src/main/java/google/NutsBoltsProblem.java
+++ src/main/java/google/NutsBoltsProblem.java
@@ -24,16 +24,14 @@ public class NutsBoltsProblem {
             if (left >= right) {
                 break;
             }
-            int mid = left + (right - 1) / 2;
-            int pivot = partition(bolts, left, right, mid, nuts[mid], String::compareTo);
-            partition(nuts, left, right, mid, bolts[pivot], String::compareTo);
+            int pivot = partition(bolts, left, right, nuts[right], comparator);
+            partition(nuts, left, right, bolts[right], comparator);
             stack.push(new int[]{left, pivot - 1});
             stack.push(new int[]{pivot + 1, right});
         }
     }
 
-    private int partition(String[] bolts, int left, int right, int pivot, String pivotValue, NBComparator comparator) {
-        swap(bolts, right, pivot);
+    private int partition(String[] bolts, int left, int right, String pivotValue, NBComparator comparator) {
         int storedIndex = left;
         for (int i = left; i <= right; i++) {
             if (comparator.compare(pivotValue, bolts[i]) < 0) {
@@ -41,7 +39,6 @@ public class NutsBoltsProblem {
                 storedIndex++;
             }
         }
-        swap(bolts, storedIndex, right);
         return storedIndex;
     }
 
